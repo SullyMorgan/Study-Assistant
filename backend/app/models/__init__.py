f8cfbe5cde6c3c0c4ef4ff533405a90434b6b97d
Horvath Attila Levente <attihorvath13@gmail.com>
2026-05-12 23:22:55 +0300

    plan generator + pdf text extractor

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .User import User
-from .Class import Class
-from .StudySession import StudySession
-from .Task import Task, TaskType
-from .UserSchedule import UserSchedule
--- backend/app/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .auth import Token, UserCreate, UserOut, UserLogin
-from .classes import ClassCreate, ClassOut
-from .tasks import TaskCreate, TaskOut, TaskType
-from .schedule import ScheduleCreate, ScheduleOut
-from .sessions import StudySessionOut, StudySessionCreate
